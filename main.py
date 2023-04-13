a6ecd39745fc00d43b6e0cd7db939660a87b785d
Vidya <118951865+Vidz2311@users.noreply.github.com>
2023-04-13 09:38:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window=Tk()
+window.geometry("300x300")
+window.title("Simple Calculator")
+
+lab1=Label(window,text="Enter 1st Number:")
+lab1.place(x=30,y=50)
+lab2=Label(window,text="Enter 2nd Number:")
+lab2.place(x=30,y=100)
