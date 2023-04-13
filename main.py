e6fefe9b68b646e8a94d940c1e1c5890a827e7ac
Vidya <118951865+Vidz2311@users.noreply.github.com>
2023-04-13 10:00:40 +0000

    Added the addition function

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from tkinter import *
 
 window=Tk()
-window.geometry("300x300")
+window.geometry("400x500")
 window.title("Simple Calculator")
 
 lab1=Label(window,text="Enter 1st Number:")
 lab1.place(x=30,y=50)
 lab2=Label(window,text="Enter 2nd Number:")
 lab2.place(x=30,y=100)
+lab3=Label(window,text="Result:")
+lab3.place(x=120,y=150)
+t1=Entry()
+t1.place(x=150,y=50)
+t2=Entry()
+t2.place(x=150,y=100)
+t3=Entry()
+t3.place(x=180,y=150)
+def add():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum=num1+num2
+  t3.insert(END, str(sum))
+b1=Button(window,text="Add",command=add)
+b1.place(x=50,y=200)
+
+
+  
